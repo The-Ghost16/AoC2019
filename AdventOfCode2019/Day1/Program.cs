90ca6679b0f9fe23e112807e5ac44b28958c7c91
Sander Notenbomer <sander.notenbomer@cybertech-int.com>
2019-12-03 16:02:47 +0100

    Completed puzzle 2.

--- AdventOfCode2019/Day1/Program.cs
+++ AdventOfCode2019/Day1/Program.cs
@@ -9,6 +9,7 @@
 
 using System;
 using System.IO;
+using System.Linq;
 
 namespace Day1
 {
@@ -25,12 +26,20 @@ namespace Day1
         /// </param>
         private static void Main(string[] args)
         {
+            var input = File.ReadAllLines("Puzzle.txt");
+
             Console.WriteLine("Hello World!");
             Console.WriteLine("Going to solve Puzzle 1!");
 
-            Puzzle1();
+            Puzzle1(input);
 
             Console.WriteLine("Puzzle 1 solved!");
+
+            Console.WriteLine("Going to solve Puzzle 2!");
+
+            Puzzle2(input);
+
+            Console.WriteLine("Puzzle 2 solved!");
 
             Console.ReadLine();
         }
@@ -38,25 +47,30 @@ namespace Day1
         /// <summary>
         /// The puzzle 1.
         /// </summary>
-        private static void Puzzle1()
+        /// <param name="lines">
+        /// The lines.
+        /// </param>
+        private static void Puzzle1(string[] lines)
         {
-            var requiredFuel = 0;
-
-            var lines = File.ReadAllLines("Puzzle1.txt");
-            foreach (var line in lines)
+            var moduleFuel = new int[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
             {
+                var line = lines[i];
                 if (int.TryParse(line, out var mass))
                 {
-                    requiredFuel += Calculate(mass);
+                    moduleFuel[i] = CalculateFuel(mass);
                 }
             }
+
+            var requiredFuel = moduleFuel.Sum();
 
             Console.WriteLine($"Required fuel: {requiredFuel}");
         }
 
         /// <summary>
         /// The calculate will calculate the required fuel for the module based on its mass.
-        /// The required fuel for the module will be calculated by: Take the mass of the module, divide by three, round down, and subtract 2.
+        /// The required fuel for the module will be calculated by: Take the mass of the module, divide by three, round down, and
+        /// subtract 2.
         /// </summary>
         /// <param name="mass">
         /// The mass.
@@ -64,13 +78,64 @@ namespace Day1
         /// <returns>
         /// The <see cref="int"/>.
         /// </returns>
-        private static int Calculate(decimal mass)
+        private static int CalculateFuel(decimal mass)
         {
             var division = mass / 3;
             var rounded = Convert.ToInt32(Math.Floor(division));
             var subtracted = rounded - 2;
 
             return subtracted;
+        }
+
+        /// <summary>
+        /// The puzzle 2.
+        /// </summary>
+        /// <param name="lines">
+        /// The lines.
+        /// </param>
+        private static void Puzzle2(string[] lines)
+        {
+            var moduleFuel = new int[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                if (int.TryParse(line, out var mass))
+                {
+                    moduleFuel[i] = CalculateFuelForModule(mass);
+                }
+            }
+
+            var requiredFuel = moduleFuel.Sum();
+
+            Console.WriteLine($"Required fuel: {requiredFuel}");
+        }
+
+        /// <summary>
+        /// The calculate fuel for module.
+        /// </summary>
+        /// <param name="mass">
+        /// The mass.
+        /// </param>
+        /// <returns>
+        /// The <see cref="int"/>.
+        /// </returns>
+        private static int CalculateFuelForModule(decimal mass)
+        {
+            var fuelForModule = CalculateFuel(mass);
+
+            var fuelForFuel = fuelForModule;
+            do
+            {
+                fuelForFuel = CalculateFuel(fuelForFuel);
+
+                if (fuelForFuel > 0)
+                {
+                    fuelForModule += fuelForFuel;
+                }
+            }
+            while (fuelForFuel > 0);
+
+            return fuelForModule;
         }
     }
 }
